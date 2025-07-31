5ff4da67c55404b1a6c32ebf5823787ae37045fb
YessetHumanMan <eset.bimanov@gmail.com>
2025-07-31 10:03:13 +0500

    fix: function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
-};
+module.exports = function reverse (n) {
+    let reversedStr = Math.abs(n).toString().split("").reverse().join("");
+      let reversedNumber = parseInt(reversedStr, 10);
+      return reversedNumber;
+  }
